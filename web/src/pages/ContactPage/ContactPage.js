7aa2807cb1521f4c4a6fcfbcfe7685b0a51ee2f7
ajf77 <ajf77@njit.edu>
2022-09-23 18:46:13 -0400

    Added text box and submit button

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,19 +1,17 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { Form, TextField, Submit } from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
-
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form onSubmit={onSubmit}>
+        <TextField name="input" />
+        <Submit>Save</Submit>
+      </Form>
     </>
   )
 }
